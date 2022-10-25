c32219f305ab5228dcc9eb77265d47db8c097fbd
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-10-25 09:00:55 +0800

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -14,6 +14,12 @@ import fetch from 'node-fetch'
 import moment from 'moment-timezone'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 return conn.sendButton(m.chat, htki + ' ' + author + ' ' + htka, `${htjava}\n${dmenub} ${conn.user.name} Source Code:\n${dmenub} ${sgh}\n${dmenuf}`, logo, [['Owner', '/owner'], ['Donasi', '/donasi']], fakes, adReply)
+SC By : arifzyn
+Recode By :RiellXd
+
+Mo sc? donasi dulu ngentot🗿
+
 }
+
 handler.command = /^sc(ript(bot)?|bot)?$/i
 export default handler
